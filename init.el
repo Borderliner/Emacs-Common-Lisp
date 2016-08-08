02ec69ae2f41fb504033cbd3755b178e7f761ad4
Borderliner <hajianpour.mr@gmail.com>
2016-08-08 14:52:09 +0430

    Added official melpa repo

--- init.el
+++ init.el
@@ -7,9 +7,12 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 ;;(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
+;;             '("tromey" . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 
 ;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 ;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
